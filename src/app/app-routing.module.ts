5f56b6901e954295a7e067575911c8fb80f436df
elyesturki <elyestu@gmail.com>
2022-03-03 02:27:47 +0100

    init project ngrx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './components/products/products.component';
-import { HomeComponent } from './components/home/home.component';
-import { AddProductComponent } from './components/add-product/add-product.component';
-import { EditProductComponent } from './components/edit-product/edit-product.component';
 
-const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "products", component: ProductsComponent},
-  {path: "new-product", component: AddProductComponent},
-  {path: "edit-product/:id", component: EditProductComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { ProductsComponent } from './components/products/products.component';
-import { HomeComponent } from './components/home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AddProductComponent } from './components/add-product/add-product.component';
-import { EditProductComponent } from './components/edit-product/edit-product.component';
-import { ProductsNavBarComponent } from './components/products/products-nav-bar/products-nav-bar.component';
-import { ProductsListComponent } from './components/products/products-list/products-list.component';
-import { ProductItemComponent } from './components/products/product-item/product-item.component';
-import { CounterStatesComponent } from './components/counter-states/counter-states.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    ProductsComponent,
-    HomeComponent,
-    AddProductComponent,
-    EditProductComponent,
-    ProductsNavBarComponent,
-    ProductsListComponent,
-    ProductItemComponent,
-    CounterStatesComponent
   ],
   imports: [
     BrowserModule,
