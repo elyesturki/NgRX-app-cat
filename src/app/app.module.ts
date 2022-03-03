82bff152bb11afae93b33fbe9d0db4b7b29fd5d1
elyesturki <elyestu@gmail.com>
2022-03-03 02:44:57 +0100

    add component necessaires, routing, html navbar, buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductsNavBarComponent } from './components/products/products-nav-bar/products-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductsComponent,
+    ProductsNavBarComponent,
   ],
   imports: [
     BrowserModule,
